ddae4ab86fedfdac5aff26c5c5a2583753a67da1
Lukz07 <gutierrez.lukas@gmail.com>
2023-07-03 12:29:49 -0300

    added get balance method

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ const validateProvider = (setWallet) => {
   }
 }
 
-const connectAccount = async (setWallet) => {
+const connectAccount = async (setWalletConnection) => {
   const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' })
     .catch((err) => {
       if (err.code === 4001) {
@@ -33,14 +33,31 @@ const connectAccount = async (setWallet) => {
     });
 
   const account = accounts[0];
-  setWallet(account);
+  setWalletConnection("Connected")
+  return account;
+}
+
+const getBalance = async (account, setWalletAddress) => {
+  const balance = await window.ethereum.request({ method: 'eth_getBalance', params: [
+    account, "latest"
+  ]});
+
+  setWalletAddress({
+    address: account,
+    balance: parseInt(balance) / Math.pow(10, 16)
+  })
+}
+
+const connectWallet = async (setWalletConnection, setWalletAddress) => {
+  connectAccount(setWalletConnection).then((account) => getBalance(account, setWalletAddress))
 }
 
 function App() {
-  const [wallet, setWallet] = useState(null)
+  const [walletConnection, setWalletConnection] = useState("Disconnected");
+  const [walletAddress, setWalletAddress] = useState("No wallet address");
 
   useEffect(() => {
-    validateProvider(setWallet);
+    validateProvider(setWalletConnection);
   }, []);
 
   return (
@@ -50,11 +67,15 @@ function App() {
           label="Connect Metamask wallet"
           primary={true}
           size="large"
-          onClick={() => connectAccount(setWallet)}
+          onClick={() => connectWallet(setWalletConnection, setWalletAddress)}
           backgroundColor="#ff6c01"
           />
         <p>
-          Wallet connection state: <code>{wallet}</code>
+          Wallet connection state: <b><code>{walletConnection}</code></b>
+        </p>
+        <p>
+          Wallet adress: <b><code>{walletAddress.address}</code></b><br/>
+          Wallet balance: <b><code>{walletAddress.balance}</code></b>
         </p>
       </div>
     </div>
